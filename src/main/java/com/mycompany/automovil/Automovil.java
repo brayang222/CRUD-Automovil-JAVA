18273b95e9a0118e1c70b219a5d3844215b7c3b2
brayang222 <116692605+brayang222@users.noreply.github.com>
2023-09-04 15:03:23 -0500

    first

---
+++ src/main/java/com/mycompany/automovil/Automovil.java
@@ -0,0 +1,16 @@
+package com.mycompany.automovil;
+
+import com.mycompany.automovil.igu.Principal;
+
+public class Automovil {
+
+    public static void main(String[] args) {
+        
+        Principal principal = new Principal();
+        principal.setVisible(true);
+        principal.setLocationRelativeTo(null);
+        
+        
+       
+    }
+}
